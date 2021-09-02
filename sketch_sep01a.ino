1b870fc442add3dafc8e2a04d4372245e78b6d5f
Anthony Li <antli.inbox@gmail.com>
2021-09-02 00:54:54 -0700

    Display 1/2.5/10

--- sketch_sep01a.ino
+++ sketch_sep01a.ino
@@ -24,11 +24,13 @@ void loop()
   {
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Dust Concentration");
-    //lcd.setCursor(0, 1);
-    //lcd.print("PM1.0 :" + String(data.PM_AE_UG_1_0) + "(ug/m3)");
+    lcd.print("PM1.0/2.5/10");
     lcd.setCursor(0, 1);
-    lcd.print("PM2.5 :" + String(data.PM_AE_UG_2_5) + "(ug/m3)");
+    lcd.print(String(data.PM_AE_UG_1_0) + "/" + String(data.PM_AE_UG_2_5) + "/" + String(data.PM_AE_UG_10_0) + " ug/m3");
+    
+    //lcd.print("PM1.0 :" + String(data.PM_AE_UG_1_0) + "(ug/m3)");
+    //lcd.setCursor(0, 1);
+    //lcd.print("PM2.5 :" + String(data.PM_AE_UG_2_5) + "(ug/m3)");
     //lcd.setCursor(0, 3);
     //lcd.print("PM10  :" + String(data.PM_AE_UG_10_0) + "(ug/m3)");
     
